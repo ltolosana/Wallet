e900a6a3010ec5b3bb25bd7b556d02ae90f45693
Fernando Rodriguez <frr@agbo.biz>
2015-06-01 22:09:52 +0200

    Initial Commit

---
+++ Wallet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wallet
+//
+//  Created by Fernando Rodríguez Romero on 01/06/15.
+//  Copyright (c) 2015 Agbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
